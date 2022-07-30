5af66728a833b396a61e9f4af3e0394561ec2df4
Victor Pena <13victor.pena@gmail.com>
2022-07-30 11:40:15 -0500

    created quotes tables as a test and msyql is working

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,5 +1,9 @@
+use codeup_test_db;
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums (
+CREATE TABLE quotes (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-
+    author_first_name VARCHAR(50) DEFAULT 'NONE',
+    author_last_name  VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    PRIMARY KEY (id)
 );
