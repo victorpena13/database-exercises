b145ac98a8b813ce2593256f42ac84072951cd47
Victor Pena <13victor.pena@gmail.com>
2022-08-12 10:08:07 -0500

    used update command to modify existing data in a table

--- albums_migration.sql
+++ albums_migration.sql
@@ -12,8 +12,19 @@ CREATE TABLE albums (
 
 INSERT INTO albums(artist, track, genre, sales, release_date)
 VALUES ('Kate Bush', 'running up that hill', 'alternative', 31, '08-05-85'),
-       ('The Muffs', 'Kid''s of America', 'punk', sales , '1995');
+       ('The Muffs', 'Kid''s of America', 'punk', sales , '1995'),
+       ('Lana Del Rey', 'chemtrail''s over the country club', 'alternative', sales, release_date),
+       (artist, 'you''re not who you were', genre, sales, release_date);
+
+
 
 SELECT 'Kate Bush', 'running up that hill' from albums;
-SELECT * from albums;
+SELECT * FROM albums;
+SELECT * FROM albums where genre = 'alternative';
+SELECT* FROM albums where id = 1;
+SELECT track FROM ALBUMS WHERE id = 4;
+
+update albums
+set artist = 'Pell'
+where id = 4;
 
