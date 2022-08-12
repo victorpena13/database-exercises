fffed302a3ceee89bc8f329586e30e8dcb7dcbe7
Victor Pena <13victor.pena@gmail.com>
2022-08-12 00:04:44 -0500

    used INSERT statment to tell MYSQL to add data, into the albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,10 @@ CREATE TABLE albums (
     artist VARCHAR(50) DEFAULT 'UNKNOWN',
     track VARCHAR(100) DEFAULT 'UNTITLED',
     genre VARCHAR(50) DEFAULT 'VARIOUS',
-    sales int,
+    sales varchar(50) DEFAULT 'UNKNOWN',
     release_date VARCHAR(50),
     PRIMARY KEY (id)
 );
+
+INSERT INTO albums(artist, track, genre, sales, release_date)
+VALUES ('Kate Bush', 'running up that hill', 31, 'alternative', '08-05-85');
