06b24569f99abe1ca3dbca5b5e88179fbde627c6
Victor Pena <13victor.pena@gmail.com>
2022-08-18 09:59:53 -0500

    updated_exercises.sql

---
+++ update_exercises.sql
@@ -0,0 +1,21 @@
+use codeup_test_db;
+
+select * from albums;
+select * from albums where artist = 'Lana Del Rey';
+select * from albums where release_date < 2000;
+
+update albums
+set release_date = release_date - 100
+where release_date < 2000;
+
+update albums
+set sales = sales * 10
+where sales != 'UNKNOWN';
+
+update albums
+set artist = 'lama del rey'
+where artist = 'Lana Del Rey';
+select * from albums where artist = 'lama del rey';
+
+
+select * from albums;
