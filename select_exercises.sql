a9f6c8eb4f53bb404b377990ea4335c252e483ee
Victor Pena <13victor.pena@gmail.com>
2022-08-18 10:47:07 -0500

    used the select statement to display the year us and them was released

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,4 @@
 use codeup_test_db;
 
- select track from albums where artist = 'Pink Floyd'
+select track from albums where artist = 'Pink Floyd';
+select release_date from albums where track = 'us and them';
