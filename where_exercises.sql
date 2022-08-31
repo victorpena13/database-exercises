8a0415e128348ce04ee66ed69143fb2ec31488d0
Victor Pena <13victor.pena@gmail.com>
2022-08-31 09:27:51 -0500

    found all employees whose last name has a q

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,9 +1,13 @@
 use employees;
 
-# select first_name
-# from employees
-# where first_name in ('Irena', 'Vidya', 'Maya');
+select first_name
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya');
 
 select last_name, first_name
 from employees
 where last_name REGEXP '^E';
+
+select last_name
+from employees
+where last_name like '%q%';
