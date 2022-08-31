b328f6df2a7163c76f8567711e11e7cac05a4e8b
Victor Pena <13victor.pena@gmail.com>
2022-08-31 10:04:45 -0500

    chaining order by clauses

--- order_by_lesson.sql
+++ order_by_lesson.sql
@@ -1,8 +1,20 @@
 use employees;
 
+# select first_name, last_name
+# from employees
+# order by last_name;
+
+# select first_name, last_name
+# from employees
+# order by last_name desc;
+
+# select first_name, last_name
+# from employees
+# order by last_name asc;
+
+
+# chaining order by clauses:
+
 select first_name, last_name
 from employees
-order by last_name;
-
-
-
+order by last_name desc, first_name asc;
