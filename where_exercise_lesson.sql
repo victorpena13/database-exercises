957505465fc2e51b5d7a9152c2216ea18ebc82f8
Victor Pena <13victor.pena@gmail.com>
2022-08-31 08:19:20 -0500

    selected multiple categories from employees table where the employee number is between certain frame

--- where_exercise_lesson.sql
+++ where_exercise_lesson.sql
@@ -1,10 +1,13 @@
 use employees;
 
-
 select * from employees where hire_date = '1985-01-01';
 
 select distinct first_name
 from employees
 where first_name like '%sus%';
+
+select emp_no, first_name, last_name
+from employees
+where emp_no between 10026 and 10082;
 
 
