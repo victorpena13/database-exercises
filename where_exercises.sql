c654ba4251d4c0d8dcee4239a582f1c60bde6953
Victor Pena <13victor.pena@gmail.com>
2022-08-31 09:34:50 -0500

    updated the query starting at line 7 to display the last names that start and end with the letter e

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,7 +6,8 @@ where first_name in ('Irena', 'Vidya', 'Maya');
 
 select last_name, first_name
 from employees
-where last_name REGEXP '^E';
+where last_name like 'E%'
+and last_name like '%E';
 
 select last_name
 from employees
