2c0dcbdbf316d331a64ae47c445d94cb3127d028
Victor Pena <13victor.pena@gmail.com>
2022-08-31 09:59:12 -0500

    created a file order_by_lesson.sql that uses the employees database and the first query displays all employees first and last name by order of last name

---
+++ order_by_lesson.sql
@@ -0,0 +1,8 @@
+use employees;
+
+select first_name, last_name
+from employees
+order by last_name;
+
+
+
--- where_exercises.sql
+++ where_exercises.sql
@@ -7,8 +7,18 @@ where first_name in ('Irena', 'Vidya', 'Maya');
 select last_name, first_name
 from employees
 where last_name like 'E%'
-and last_name like '%E';
+    or last_name like '%E';
+
+select last_name, first_name
+from employees
+where last_name like 'E%'
+   and last_name like '%E';
 
 select last_name
 from employees
 where last_name like '%q%';
+
+select last_name
+from employees
+where last_name like '%q%'
+    and last_name not like '%qu%';
