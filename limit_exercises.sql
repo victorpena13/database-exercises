7b1725c9495c52a834f731bc4f5a0439a04b54dd
Victor Pena <13victor.pena@gmail.com>
2022-09-01 14:26:38 -0500

    created a query to get the top 5 salaries and display just the employees number from the salaries table

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,8 +1,13 @@
 use employees;
 
 
-select distinct last_name
-from employees
-where last_name like 'Z%'
-order by last_name desc
-limit 10;
+# select distinct last_name
+# from employees
+# where last_name like 'Z%'
+# order by last_name desc
+# limit 10;
+
+select emp_no
+from salaries
+order by salary desc
+limit 5;
--- limit_lesson.sql
+++ limit_lesson.sql
@@ -4,3 +4,4 @@ select emp_no, first_name, last_name
 from employees
 where first_name like 'M%'
 limit 25 offset 50;
+
