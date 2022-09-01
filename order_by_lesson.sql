e831acd9f3eff9498524560c0aa9e7cdc7b6b6ae
Victor Pena <13victor.pena@gmail.com>
2022-08-31 22:46:59 -0500

    created a new file called order_by_exercises.sql

--- order_by_lesson.sql
+++ order_by_lesson.sql
@@ -1,16 +1,16 @@
 use employees;
 
-# select first_name, last_name
-# from employees
-# order by last_name;
+select first_name, last_name
+from employees
+order by last_name;
 
-# select first_name, last_name
-# from employees
-# order by last_name desc;
+select first_name, last_name
+from employees
+order by last_name desc;
 
-# select first_name, last_name
-# from employees
-# order by last_name asc;
+select first_name, last_name
+from employees
+order by last_name asc;
 
 
 # chaining order by clauses:
