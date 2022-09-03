48bbe7533c87b9c9ff09f1525719784ee91491ff
Victor Pena <13victor.pena@gmail.com>
2022-09-02 23:11:50 -0500

    created a query that finds all employees hired in the 90s and born on dec 25

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,32 +1,32 @@
 use employees;
 
-# select distinct first_name, last_name
-# from employees
-# where first_name in ('Irena', 'Vidya', 'Maya')
-# order by last_name, first_name;
-#
-# select concat(last_name,' ', first_name)
-# from employees
-# where last_name like 'E%'
-#    or last_name like '%E';
-#
-# select emp_no, last_name, first_name
-# from employees
-# where last_name like 'E%'
-#   and last_name like '%E'
-# order by emp_no asc;
-#
-# select last_name
-# from employees
-# where last_name like '%q%';
-#
-# select last_name
-# from employees
-# where last_name like '%q%'
-#   and last_name not like '%qu%';
+select distinct first_name, last_name
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+order by last_name, first_name;
+
+select concat(last_name,' ', first_name)
+from employees
+where last_name like 'E%'
+   or last_name like '%E';
+
+select emp_no, last_name, first_name
+from employees
+where last_name like 'E%'
+  and last_name like '%E'
+order by emp_no asc;
+
+select last_name
+from employees
+where last_name like '%q%';
+
+select last_name
+from employees
+where last_name like '%q%'
+  and last_name not like '%qu%';
 
 select concat(first_name, ' ', last_name, ' - ', birth_date)
 from employees
-where year(birth_date) between 1990 and 1999
+where year(hire_date) between 1990 and 1999
     and month(birth_date) = 12
     and day(birth_date) = 25;
