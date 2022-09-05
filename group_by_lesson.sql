47d651a485d9ba53321797c4e791ebc25ae75e08
Victor Pena <13victor.pena@gmail.com>
2022-09-05 17:47:43 -0500

    multiple columns with group by

--- group_by_lesson.sql
+++ group_by_lesson.sql
@@ -6,3 +6,10 @@ from employees;
 select first_name
 from employees
 group by first_name;
+
+# multiple columns:
+select last_name, first_name
+from employees
+group by last_name, first_name;
+## the above query will show us all of the unique combinations of first and last names,
+# grouped by their last name sorted alphabetically, and within each last name group, sorted by first name.
