6beaa9885a932c725ec0aadc2353cf30d3f5fcc0
Victor Pena <13victor.pena@gmail.com>
2022-09-06 18:17:04 -0500

    aggregate and count functions

--- group_by_lesson.sql
+++ group_by_lesson.sql
@@ -11,5 +11,18 @@ group by first_name;
 select last_name, first_name
 from employees
 group by last_name, first_name;
-## the above query will show us all of the unique combinations of first and last names,
+# the above query will show us all of the unique combinations of first and last names,
 # grouped by their last name sorted alphabetically, and within each last name group, sorted by first name.
+
+
+# aggregate functions:
+# works with all data across all the rows in our result.
+# count function
+
+select count(*)
+from employees;
+
+select count(first_name)
+from employees
+where first_name not like '%a%';
+
