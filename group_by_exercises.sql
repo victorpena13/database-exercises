9c25ae50b66bb2cc089c25df08b34430149edfbf
Victor Pena <13victor.pena@gmail.com>
2022-09-10 20:12:09 -0500

    created a query to find unique last names with the letter q but not like qu

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -14,3 +14,12 @@ where last_name like 'E%'
     and last_name like '%E'
     group by last_name;
 
+select last_name, count(last_name)
+from employees
+where last_name like '%Q%'
+    and last_name not like 'QU%'
+    group by last_name;
+
+
+
+
