5b41a54ca0bb58995efeaecda8915e76a8724afa
Victor Pena <13victor.pena@gmail.com>
2022-10-04 11:12:34 -0500

    created database if not exists join_test_db | created a table roles that can be described with a name and primary key | created a table users that can be described with a name, email and a foreign key for role_id

--- joins_lesson.sql
+++ joins_lesson.sql
@@ -1,7 +1,6 @@
 create database if not exists join_test_db;
 use join_test_db;
 
-
 create table roles (
     id int unsigned not null auto_increment,
     name varchar(100) not null,
@@ -17,27 +16,3 @@ create table users (
     foreign key (role_id) references roles (id)
 );
 
-insert into roles (name) values ('admin');
-insert into roles (name) values ('author');
-insert into roles (name) values ('reviewer');
-insert into roles (name) values ('commenter');
-
-insert into users(name, email, role_id) values
-('bob', 'bob@example.com', 1),
-('joe', 'joe@example.com', 2),
-('sally', 'sally@example.com', 3),
-('adam', 'adam@example.com', 3),
-('jane', 'jane@example.com', null),
-('mike', 'mike@example.com', null);
-
-
-select users.name as user_name, roles.name as role_name
-from users
-join roles on users.role_id = roles.id;
-
-#left join - is used when you want to retrieve all the records from the left table, regardless of whether or not they have a corresponding record in the right table.
-
-select users.name as user_name, roles.name as role_name
-from users
-left join roles on users.role_id = roles.id;
-#now all users will be displayed.
