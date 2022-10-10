3a7809276761c16a31d3288e9a2ea5564d811766
Victor Pena <13victor.pena@gmail.com>
2022-10-09 23:14:49 -0500

    found all managers that are female

--- join_exercises.sql
+++ join_exercises.sql
@@ -7,3 +7,11 @@ FROM employees as e
          JOIN departments as d
               ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01';
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees as e
+         JOIN dept_manager as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' and gender = 'F';
