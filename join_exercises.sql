fd5527070ca4dcf2fb66b1b532998ec8d6e90313
Victor Pena <13victor.pena@gmail.com>
2022-10-10 11:48:08 -0500

    gathered all the titles from the customer service department and the amount of staff in each department

--- join_exercises.sql
+++ join_exercises.sql
@@ -15,3 +15,24 @@ FROM employees as e
          JOIN departments as d
               ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01' and gender = 'F';
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name, t.title
+FROM employees as e
+         JOIN dept_manager as de
+              ON de.emp_no = e.emp_no
+         JOIN titles as t
+              on t.emp_no = de.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' and gender = 'F';
+
+SELECT distinct t.title, count(t.title) as 'Count'
+FROM employees as e
+         JOIN titles as t
+              on t.emp_no = e.emp_no
+         JOIN dept_emp as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE t.to_date = '9999-01-01' and de.to_date = '9999-01-01' and d.dept_name = 'Customer Service'
+group by title;
