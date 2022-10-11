58f91fe8d69f3fe988d2691204bbb03b4243d66d
Victor Pena <13victor.pena@gmail.com>
2022-10-10 22:09:55 -0500

    found the current salary of all curent managers

--- join_exercises.sql
+++ join_exercises.sql
@@ -24,7 +24,7 @@ FROM employees as e
               on t.emp_no = de.emp_no
          JOIN departments as d
               ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' and gender = 'F';
+WHERE de.to_date = '9999-01-01';
 
 SELECT distinct t.title, count(t.title) as 'Count'
 FROM employees as e
@@ -36,3 +36,13 @@ FROM employees as e
               ON d.dept_no = de.dept_no
 WHERE t.to_date = '9999-01-01' and de.to_date = '9999-01-01' and d.dept_name = 'Customer Service'
 group by title;
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_manager, d.dept_name, s.salary
+FROM employees as e
+         JOIN dept_manager as de
+              ON de.emp_no = e.emp_no
+         JOIN salaries as s
+              on s.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' and s.to_date = '9999-01-01';
